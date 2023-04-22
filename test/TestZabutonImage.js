c9d1fede5e897cceba0ef609d31b5eb7a9a33866
haruxx <harukatarotaro@gmail.com>
2023-04-22 14:00:12 +0900

    zabutonImage test support get/set function

--- test/TestZabutonImage.js
+++ test/TestZabutonImage.js
@@ -11,7 +11,6 @@ describe("ZabutonImage", function () {
       za = await ZabutonImage.deploy();
     });
   
-    
     describe("Deployment", function () {
       
       it("Should return Image ", async function () {
@@ -19,6 +18,37 @@ describe("ZabutonImage", function () {
           const image = await za.getImage(i);
           const filename = "tmp/za" + (i) + ".svg";
           fs.writeFileSync(filename, image);
-    }});
+      }});
+    
+      it("Should Update svgHeader ", async function () {
+        const newSvgHeader = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>';
+        await za.setSvgHeader(newSvgHeader);
+        const svgHeader = await za.getSvgHeader();
+        expect(svgHeader).to.equal(newSvgHeader);
+      });
+
+      it("Should Update zabuton svgBlock ", async function () {
+        const newZabuton = '<defs><g id="a"><path d="m646 589-87></g></defs>';
+        await za.setZabuton(newZabuton);
+        const zabuton = await za.getZabuton();
+        expect(zabuton).to.equal(newZabuton);
+      });
+
+      it("Should Update frame svgBlock ", async function () {
+        const newFrame = '<path fill="#40FF5C" d="M57 58h5v10h5v9h4V49h5v47H66v-9h-4V77"/>';
+        await za.setFrame(newFrame);
+        const frame = await za.getFrame();
+        expect(frame).to.equal(newFrame);
+      });
+
+      it("Change color with svgHeader ", async function () {
+        const newSvgHeader = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1080 1080" style="background-color: yellow;">';
+        await za.setSvgHeader(newSvgHeader);
+        const image = await za.getImage(5);
+        const filename = "tmp/yellowBack.svg";
+        fs.writeFileSync(filename, image);
+      });
+
     });
+
   });
